142f9fb34c5e375b71f784a519ce1121e1ae492b
angburmun <angburmun@gmail.com>
2025-05-12 16:26:03 +0200

    Corregido error de la cámara (izq cambiado con derecha)

--- camara.gd
+++ camara.gd
@@ -15,9 +15,9 @@ func _process(delta):
 	var horizontal_input := 0.0
 	var vertical_input := 0.0
 
-	if Input.is_action_pressed("camara_derecha"):
-		horizontal_input -= 1
 	if Input.is_action_pressed("camara_izquierda"):
+		horizontal_input -= 1
+	if Input.is_action_pressed("camara_derecha"):
 		horizontal_input += 1
 	if Input.is_action_pressed("camara_arriba"):
 		vertical_input -= 1
