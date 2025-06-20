5ab3fb6d52dcfd985287a720999750a89512fcea
angburmun <angburmun@gmail.com>
2025-06-20 19:04:24 +0200

    Reestructurados archivos. Añadida animación y colisión de la puerta. Añadida colision con el aparador.

--- SCRIPTS/puerta.gd
+++ SCRIPTS/puerta.gd
@@ -1,8 +1,8 @@
-extends Node3D
+extends CSGMesh3D
 
 var abierto = false
 var en_movimiento = false
-@onready var animation_player = $"../animation_godot"
+@onready var animation_player = $"../../animaciones"
 
 func interact():
 	if !en_movimiento:
@@ -10,9 +10,9 @@ func interact():
 		abierto = !abierto
 		
 		if abierto:
-			animation_player.play("abrir")
+			animation_player.play("animation_open")
 		else:
-			animation_player.play("cerrar")
+			animation_player.play("animation_close")
 		await get_tree().create_timer(1.0, false).timeout
 		
 		en_movimiento = false
