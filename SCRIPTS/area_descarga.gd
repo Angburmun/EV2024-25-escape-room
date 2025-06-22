94d22abbb4da89ec772d4f433b4f04305af41967
angburmun <angburmun@gmail.com>
2025-06-22 23:52:52 +0200

    Implementado mejor sistema de descarga de habitaciones

--- SCRIPTS/area_descarga.gd
+++ SCRIPTS/area_descarga.gd
@@ -1,8 +1,10 @@
 extends Area3D
 
 @onready var area_pos_z: float = global_transform.origin.z
-@onready var habitacion1 = $"../../habitacion1"
-@onready var habitacion2 = $"../../habitacion2"
+@onready var habitacion_1 = $"../../habitacion1"
+@onready var habitacion_2 = $"../../habitacion2"
+@export var ruta_habitacion1: String = "res://ESCENAS/habitacion_1.tscn"
+@export var ruta_habitacion2: String = "res://ESCENAS/habitacion_2.tscn"
 
 func _ready():
 	connect("body_entered", _on_body_entered)
@@ -11,21 +13,25 @@ func _ready():
 func _on_body_entered(body):
 	if body.name == "jugador":
 		var jugador_pos_z = body.global_transform.origin.z
-		print("Jugador Z: ", jugador_pos_z)
-		print("Area Z:    ", area_pos_z)
+		#print("Jugador Z: ", jugador_pos_z)
+		#print("Area Z:    ", area_pos_z)
 		
 		if  jugador_pos_z > area_pos_z:
-			habitacion1.visible = false
+			habitacion_1.queue_free()
 		else:
-			habitacion2.visible = false
+			habitacion_2.queue_free()
 
 func _on_body_exited(body):
 	if body.name == "jugador":
 		var jugador_pos_z = body.global_transform.origin.z
-		print("Jugador Z: ", jugador_pos_z)
-		print("Area Z:    ", area_pos_z)
+		#print("Jugador Z: ", jugador_pos_z)
+		#print("Area Z:    ", area_pos_z)
 		
 		if  jugador_pos_z > area_pos_z:
-			habitacion1.visible = true
+			var escena1 = load(ruta_habitacion1) as PackedScene
+			habitacion_1 = escena1.instantiate()
+			get_parent().get_parent().add_child(habitacion_1)
 		else:
-			habitacion2.visible = true
+			var escena2 = load(ruta_habitacion2) as PackedScene
+			habitacion_2 = escena2.instantiate()
+			get_parent().get_parent().add_child(habitacion_2)
