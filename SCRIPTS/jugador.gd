310d45a6763ec7d6bb037da8e10dfd20c2fac2d5
angburmun <angburmun@gmail.com>
2025-06-22 20:26:02 +0200

    Añadidas texturas de paredes, techo y puertas. Añadida habilidad para agacharse

--- SCRIPTS/jugador.gd
+++ SCRIPTS/jugador.gd
@@ -4,13 +4,20 @@ const WALK_SPEED = 3.5
 const JUMP_VELOCITY = 4.0
 const SENSITIVITY = 0.8
 const SENSITIVITY_DIVIDER = 800
+var speed = WALK_SPEED
 
-#fov variables
+# crouch
+const CROUCH_HEIGHT = -0.15
+const STAND_HEIGHT = 0.75
+const CROUCH_SPEED = 2.25
+var is_crouching = false
+var target_y = STAND_HEIGHT
+
+# fov
 const BASE_FOV = 75.0
 const FOV_CHANGE = 1.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var head = $cabeza
 @onready var camera = $cabeza/camara
@@ -31,8 +38,17 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle Jump.
+	
+	# Handle crouch
+	is_crouching = Input.is_action_pressed("crouch")
+	if is_crouching:
+		speed = CROUCH_SPEED
+		target_y = CROUCH_HEIGHT
+	else:
+		speed = WALK_SPEED
+		target_y = STAND_HEIGHT
+		
+	# Handle Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -41,18 +57,19 @@ func _physics_process(delta):
 	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
-			velocity.x = direction.x * WALK_SPEED
-			velocity.z = direction.z * WALK_SPEED
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
 		else:
-			velocity.x = lerp(velocity.x, direction.x * WALK_SPEED, delta * 10.0)
-			velocity.z = lerp(velocity.z, direction.z * WALK_SPEED, delta * 10.0)
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 10.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 10.0)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * WALK_SPEED, delta * 3.0)
-		velocity.z = lerp(velocity.z, direction.z * WALK_SPEED, delta * 3.0)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 	
 	# FOV
 	var velocity_clamped = clamp(velocity.length(), 0.5, WALK_SPEED * 2)
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	head.position.y = lerp(head.position.y, target_y, delta * 10.0)
 	
 	move_and_slide()
