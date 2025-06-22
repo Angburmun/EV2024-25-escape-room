a649338b7b38e8ae6284851d1f2d3c955c6fb89d
angburmun <angburmun@gmail.com>
2025-06-22 23:37:06 +0200

    Arreglada descarga de habitaciones.

--- SCRIPTS/area_descarga.gd
+++ SCRIPTS/area_descarga.gd
@@ -6,14 +6,26 @@ extends Area3D
 
 func _ready():
 	connect("body_entered", _on_body_entered)
+	connect("body_exited", _on_body_exited)
 
 func _on_body_entered(body):
 	if body.name == "jugador":
 		var jugador_pos_z = body.global_transform.origin.z
+		print("Jugador Z: ", jugador_pos_z)
+		print("Area Z:    ", area_pos_z)
 		
 		if  jugador_pos_z > area_pos_z:
 			habitacion1.visible = false
-			habitacion2.visible = true
 		else:
-			habitacion1.visible = true
 			habitacion2.visible = false
+
+func _on_body_exited(body):
+	if body.name == "jugador":
+		var jugador_pos_z = body.global_transform.origin.z
+		print("Jugador Z: ", jugador_pos_z)
+		print("Area Z:    ", area_pos_z)
+		
+		if  jugador_pos_z > area_pos_z:
+			habitacion1.visible = true
+		else:
+			habitacion2.visible = true
