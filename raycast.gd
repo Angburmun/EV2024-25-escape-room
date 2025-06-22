b2d1096a7af77b4fec3b1c6c72906e7805759dfb
angburmun <angburmun@gmail.com>
2025-06-22 20:47:20 +0200

    Reestructuración

--- raycast.gd
+++
@@ -1,8 +0,0 @@
-extends RayCast3D
-
-func _process(_delta):
-	if is_colliding():
-		var hitObj = get_collider()
-		
-		if hitObj.has_method("interact") && Input.is_action_just_pressed("interact"):
-			hitObj.interact()
